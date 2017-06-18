c896ea4b72e54012f850fea30c73083d38383f21
Tatyana <tanysha-kyzn@mail.ru>
2017-06-19 03:19:00 +0500

    ka

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,3 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
 ]
-if settings.DEBUG:
-    if settings.MEDIA_ROOT:
-        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += staticfiles_urlpatterns()
